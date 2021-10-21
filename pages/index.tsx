9c84198b94e4a2cc5261afee65d2bbd0a9c9dfe5
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-10-20 21:43:20 -0400

    big package.json changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import { useState } from "react";
-import { useContext } from "react";
+import { useAuth } from "context/useAuth";
+import { useEffect } from "react";
 import Link from "next/link";
-import {auth} from "../firebase/clientApp";
-
-
+import Router from "next/router";
 const Home: NextPage = () => {
-  const [userName, setuserName] = useState("");
-  const [password, setPassword] = useState("");
+  const { currentUser } = useAuth();
+  useEffect(() => {
+    if (currentUser) {
+      Router.push("/userDashboard");
+    } else {
+      Router.push("/login");
+    }
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center h-screen text-4xl">
       <h1>Munkey</h1>
--- pages/login.tsx
+++ pages/login.tsx
@@ -2,9 +2,6 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
 import { useState } from "react";
 import Router from "next/router";
 import { useAuth } from "context/useAuth";
@@ -26,35 +23,23 @@ const Login: NextPage = (event) => {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-4xl">
       <form className="w-96" onSubmit={addUser}>
-        <Stack spacing={2}>
-          <h1>Login</h1>
-          <TextField
-            id="outlined-basic"
-            label="email"
-            variant="outlined"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-          <TextField
-            margin="normal"
-            fullWidth
-            name="password"
-            label="password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />{" "}
-          <Button variant="contained" type="submit">
-            Login
-          </Button>
-          <Button variant="outlined">
-            <Link href="/register">
-              <a>Register</a>
-            </Link>
-          </Button>
-        </Stack>
+        <h1 className="text-5xl ">Munkey</h1>
+        <input
+          type="text"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit">Login</button>
+        <button>
+          <Link href="/register">
+            <a>Register</a>
+          </Link>
+        </button>
       </form>
     </div>
   );
